37c9b16107107e46b7c1f78a9c4df373c18b6f3f
geniotechrobotics <geniotechrobotics@gmail.com>
2026-09-19 13:34:12 -0400

    did_fizzbuzz

---
+++ src/Footlong.py
@@ -0,0 +1,63 @@
+#Mrleast = 1
+y = 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+h = 40
+x = 2
+
+
+
+
+game = "Mr.least wants a 10 inch footlong"
+food = "so he went to subway to buy"
+garbage = "1 footlong but they told him to leave"
+mr_p = "because he loves footlongs so much that"
+hello = "subways go bankrupt because of him"
+
+list_foods = ["Pizza","Grilled cheese","Veggies","Stuff"]
+list_footlongs = ["4inch","6inch"]
+
+print(game,food,garbage,mr_p,hello)
+
+if y == 10:
+    if h > 50:
+        print("all footlongs are gone")
+    elif h + y <= 60:
+        if x >= 1000:
+            print("Mr.Peast screams in terror")
+        else:
+            print("then faints because")
+    else:
+        print("someone makes a footlong")
+else:
+    print("then burns it like a psyco")
+
+
+
+
+for i in range(1,101):
+    if i % 2 == 0:
+        print(i)
+
+for i in range(20):
+    if i % 3 ==0 and i % 5 == 0:
+        print("fizzbuzz", i)
+    elif i % 3 == 0:
+        print("fizz", i)
+    elif i % 5 == 0:
+        print("buzz", i)
+    
+    else:
+        print(0)
